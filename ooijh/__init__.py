cbec42426dde78b0c97a979d9e118a1f843ba41a
Ian Black <blackia@oregonstate.edu>
2023-12-23 19:52:52 -0800

    Added sensor classes to __init__.

--- ooijh/__init__.py
+++ ooijh/__init__.py
@@ -10,3 +10,9 @@ from ooijh.instruments.phsen import PHSEN
 from ooijh.instruments.pco2 import PCO2
 from ooijh.instruments.vel3d import VEL3D
 from ooijh.instruments.optaa import OPTAA
+from ooijh.instruments.wavss import WAVSS
+from ooijh.instruments.fdchp import FDCHP
+from ooijh.instruments.zplsc import ZPLSC
+from ooijh.instruments.camds import CAMDS
+from ooijh.instruments.mopak import MOPAK
+from ooijh.instruments.parad import PARAD
